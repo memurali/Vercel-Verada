874a1590f342cef8af26416414d736e18f3871e6
Srimsd77 <126162438+Srimsd77@users.noreply.github.com>
2025-05-15 16:54:34 +0530

    Delete apps directory

--- apps/waste_source_group/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WasteSourceGroupConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.waste_source_group'
-    verbose_name = 'Waste Source Group'
