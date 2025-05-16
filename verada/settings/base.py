fddd5eca1801505f3d0b36702eb033ebbf5e820a
Srimsd77 <126162438+Srimsd77@users.noreply.github.com>
2025-05-16 11:14:01 +0530

    Update base.py

--- verada/settings/base.py
+++ verada/settings/base.py
@@ -149,8 +149,9 @@ USE_TZ = True
 #STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'public', 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'public', 'static')]
+#STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # Where collectstatic
 
 
 # Media files
