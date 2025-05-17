0fbc1cf441f46933145a54ea931604d9c7a3438a
Srimsd77 <126162438+Srimsd77@users.noreply.github.com>
2025-05-17 13:37:39 +0530

    Update base.py

--- verada/settings/base.py
+++ verada/settings/base.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
