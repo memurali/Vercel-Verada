405881a35d59112e857a187e5ecc6efb5c193725
Srimsd77 <126162438+Srimsd77@users.noreply.github.com>
2025-05-20 13:26:49 +0530

    Update base.py

--- verada/settings/base.py
+++ verada/settings/base.py
@@ -99,8 +99,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'neondb',
         'USER': 'neondb_owner',
-        'PASSWORD': 'npg_GBjnT2WqOhY3',
-        'HOST': 'ep-purple-meadow-a4no1r5c.us-east-1.aws.neon.tech',
+        'PASSWORD': 'npg_z4ePYXSFKI8E',
+        'HOST': 'ep-divine-dawn-a4p584jp-pooler.us-east-1.aws.neon.tech',
         'PORT': '5432',
     },
     'sqlite': {
