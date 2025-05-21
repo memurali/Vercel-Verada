f4e62255f4c5102dd0310a10a2a675ccc86fb347
Srimsd77 <126162438+Srimsd77@users.noreply.github.com>
2025-05-21 15:42:33 +0530

    Update async_email.py

--- apps/common/services/async_email.py
+++ apps/common/services/async_email.py
@@ -1,5 +1,8 @@
 import threading
 from apps.common.services.email_service import EmailService
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
 
 class AsyncEmailSender(threading.Thread):
     def __init__(self, subject, to_email, template_name, context):
@@ -9,10 +12,20 @@ class AsyncEmailSender(threading.Thread):
         self.context = context
         threading.Thread.__init__(self)
 
+    # def run(self):
+    #     EmailService.send_email(
+    #         subject=self.subject,
+    #         to_email=self.to_email,
+    #         template_name=self.template_name,
+    #         context=self.context
+    #     )
+
     def run(self):
-        EmailService.send_email(
+        message = render_to_string(self.template_name, self.context)
+        email = EmailMessage(
             subject=self.subject,
-            to_email=self.to_email,
-            template_name=self.template_name,
-            context=self.context
+            to=[self.to_email],
+            body=message,
         )
+        email.content_subtype = 'html'
+        email.send()
