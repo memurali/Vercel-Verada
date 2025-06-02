59ea47457f0ecc5e3fdc3441444fb9781c9d14da
Murali <murali@progressive-solution.com>
2025-06-02 15:36:04 +0530

    Update pickup_views.py

--- apps/waste_generators/pickup_views.py
+++ apps/waste_generators/pickup_views.py
@@ -16,6 +16,8 @@ from django.http import JsonResponse, HttpResponse
 from apps.waste_collectors.models import Collector
 from datetime import datetime, timedelta
 from django.apps import apps
+from django.conf import settings
+
 
 
 
@@ -62,6 +64,26 @@ def get_pickup_food_type(request, group_id):
     return JsonResponse({'commodities': commodities})
 
 
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+
+def upload_file_to_s3_fileobj(file_obj, s3_key):
+    s3 = boto3.client(
+        's3',
+        region_name=getattr(settings, 'AWS_S3_REGION_NAME', 'us-east-1'),
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+    )
+    try:
+        s3.upload_fileobj(file_obj, settings.AWS_STORAGE_BUCKET_NAME, s3_key)
+        s3_url = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/{s3_key}"
+        return s3_url
+    except NoCredentialsError:
+        return None
+
+
+
 @require_POST
 @transaction.atomic
 def submit_waste_pickup(request):
@@ -75,6 +97,10 @@ def submit_waste_pickup(request):
         address =  WasteSourceMaster.objects.get(id=request.POST.get("address"))
         image = request.FILES.get("upload_file")
 
+        # Upload file to S3 and get the URL
+        s3_key = f"waste_pickups/{image.name}"
+        s3_url = upload_file_to_s3_fileobj(image, s3_key)
+
         waste_source = WasteSource.objects.create(
             waste_source=source,
             waste_type=waste_type,
@@ -86,7 +112,7 @@ def submit_waste_pickup(request):
             pickup_date=pickup_date,
             waste_source=waste_source,
             address=address.address,
-            image=image,
+            image=s3_url,
             destination=destination
         )
 
