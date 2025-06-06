d65e5e9cbe142805c817b317541d5fb18717636d
Murali <murali@progressive-solution.com>
2025-06-06 13:12:02 +0530

    Update pickup_views.py

--- apps/waste_generators/pickup_views.py
+++ apps/waste_generators/pickup_views.py
@@ -19,6 +19,24 @@ from django.apps import apps
 from django.conf import settings
 
 
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+
+def upload_file_to_s3_fileobj(file_obj, s3_key):
+    s3 = boto3.client(
+        's3',
+        region_name=getattr(settings, 'AWS_S3_REGION_NAME', 'us-east-1'),
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+    )
+    try:
+        s3.upload_fileobj(file_obj, settings.AWS_STORAGE_BUCKET_NAME, s3_key)
+        s3_url = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/{s3_key}"
+        return s3_url
+    except NoCredentialsError:
+        return None
+
 
 
 @login_required(login_url='login')
@@ -77,6 +95,9 @@ def submit_waste_pickup(request):
         address =  WasteSourceMaster.objects.get(id=request.POST.get("address"))
         image = request.FILES.get("upload_file")
 
+        s3_key = f"waste_pickups/{image.name}"
+        s3_url = upload_file_to_s3_fileobj(image, s3_key)
+
         waste_source = WasteSource.objects.create(
             waste_source=source,
             waste_type=waste_type,
@@ -88,7 +109,7 @@ def submit_waste_pickup(request):
             pickup_date=pickup_date,
             waste_source=waste_source,
             address=address.address,
-            image=image,
+            image=s3_url,
             destination=destination
         )
 
